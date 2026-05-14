af8ae920f9f35fe574e77c935d21eccb9fb9dfea
FelipeLima78 <felipe1905lima@gmail.com>
2026-05-14 12:55:15 -0300

    feat(Cadastro) - Criação da função do cadastro, tela funcional

---
+++ app/src/main/java/com/network/plantfyth/retrofit/RetroFitService.java
@@ -0,0 +1,24 @@
+package com.network.plantfyth.retrofit;
+
+import com.google.gson.Gson;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetroFitService {
+    private Retrofit retrofit;
+
+    public RetroFitService(){
+        initializeRetroFit();
+    }
+    private void initializeRetroFit(){
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.1.101:8080/")
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .build();
+    }
+
+    public Retrofit getRetrofit() {
+        return retrofit;
+    }
+}
